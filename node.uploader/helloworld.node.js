168fba16c43236c8f17bb3a9f3f37bc76c091dfb
vasnake <vasnake@gmail.com>
2014-11-08 20:41:58 +0300

    whitespaces fixed

--- node.uploader/helloworld.node.js
+++ node.uploader/helloworld.node.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
 var server = http.createServer(
-	function (request, response) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.end('Hello World\n');
-		console.log('Page requested at ' + new Date() + ', for ' + request.url);
-	}
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World\n');
+        console.log('Page requested at ' + new Date() + ', for ' + request.url);
+    }
 );
 
 server.listen(1337, "0.0.0.0");
